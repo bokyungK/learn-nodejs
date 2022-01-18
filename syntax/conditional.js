7ee784488e2941a0da95716aa36df135a33d0be1
bokyungK <freebob2b@gmail.com>
2022-01-18 19:27:05 +0900

    feat: Learn about synchronous and Asynchronous

--- syntax/conditional.js
+++
@@ -1,7 +0,0 @@
-var args = process.argv;
-
-if (args[2]) {
-  console.log('A');
-} else {
-  console.log('B');
-}
--- syntax/function.js
+++
@@ -1,9 +0,0 @@
-// Use function's Output
-
-// Create function
-function sum(first, second) {
-  return first + second; // return은 값을 출력한다는 의미와, 함수를 종료한다는 두 가지 의미를 가지고 있다.
-}
-
-// Call function
-console.log(sum(2, 4)); // return 키워드로 전달받은 함수의 출력값으로, 화면에 보여주거나 연산에 이용하는 등 다양하게 활용할 수 있다.
