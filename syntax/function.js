5cac97432d54e817b825377d5e29ab0ce5734309
bokyungK <freebob2b@gmail.com>
2022-01-18 17:33:02 +0900

    feat: Learn about function and input

---
+++ syntax/function.js
@@ -0,0 +1,12 @@
+// Use function's input
+
+// Create function
+function sum(first, second) { // 입력 값와 함수를 연결하는 매개 변수(parameter)가 필요
+  console.log(first + second);
+}
+
+// Call function
+sum(2, 4) // 입력 값(argument)에 따라 함수가 다르게 동작
+
+
+// 입력 값을 이용하면 함수가 더 똑똑해져서 다양하고 복잡한 기능을 수행할 수 있게 된다.
