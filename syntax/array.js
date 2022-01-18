a69dac952800883a633efda51ac2ed8d8e939863
bokyungK <freebob2b@gmail.com>
2022-01-18 16:18:47 +0900

    feat: Learn about array's crud

---
+++ syntax/array.js
@@ -0,0 +1,19 @@
+// 배열의 CRUD를 다룰 줄 알아야 한다.
+
+// Create
+var arr = ['A', 3, 'C', 'D'];
+
+// Read
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+// Update
+arr[1] = 'B';
+arr.push('E');
+
+// Delete
+arr.pop();
+
+console.log(arr);
